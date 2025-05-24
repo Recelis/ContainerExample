e356ff74739c34e3eb8f5111f0e0550394ef877c
Recelis <jackymlui@live.com>
2025-05-24 17:42:37 +1000

    setting up docker-compose for dev and prod environments for base apps

--- api/src/index.ts
+++ api/src/index.ts
@@ -6,10 +6,34 @@ import express, {
 } from "express";
 import "dotenv/config";
 
+import cors from "cors";
+
 const api: Application = express();
 
+api.use(express.json());
+
+api.use(
+  cors({
+    // TODO: conditionally allow the origins based on the environment
+    origin:
+      process.env.NODE_ENV === "prod"
+        ? ["http://localhost:3000"]
+        : ["http://localhost"],
+    credentials: true, // if you're using cookies or HTTP auth}))
+  })
+);
+
 api.get("/", (_req: Request, res: Response) => {
-  res.send("Successful response.");
+  res.send("Successful response");
 });
 
-export default api;
+const port = process.env["PORT"];
+const server = () => {
+  api.listen(port || 8000, () =>
+    console.log(`App is listening on port ${port ?? 8000}.`)
+  );
+};
+
+server();
+
+export default server;
--- api/src/lambda.ts
+++
@@ -1,4 +0,0 @@
-// lambda.js
-import * as serverlessExpress from "@codegenie/serverless-express";
-import app from "./index";
-exports.handler = serverlessExpress.configure({ app });
